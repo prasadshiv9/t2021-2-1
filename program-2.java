0fe7da73350486f210a8187ebf57c21702bf9cfa
prasadshiv9 <106333515+prasadshiv9@users.noreply.github.com>
2022-09-27 08:54:35 +0530

    Update program-2.java

--- program-2.java
+++ program-2.java
@@ -1,1 +1,24 @@
 
+import java.util.Scanner;
+
+public class Problem2 {
+
+	public static void main(String[] args) {
+
+		Scanner scan =new Scanner(System.in);
+		
+		System.out.println("How long series do you want?");
+		System.out.println("Enter the number");
+		int a=scan.nextInt();
+		
+		int b=1;
+		int x=2;
+		for (int i = 1; i <=a; i++) {
+			System.out.print(b+" ");
+			b=x+b;
+			
+		}
+	}
+
+}
+
