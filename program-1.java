15b92f4b4d7640e6cfd4e11066bc727cbe791eda
prasadshiv9 <106333515+prasadshiv9@users.noreply.github.com>
2022-09-27 08:53:51 +0530

    Update program-1.java

--- program-1.java
+++ program-1.java
@@ -1,1 +1,44 @@
+import java.util.Scanner;
+
+public class Problem1 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		double a;
+		double b;
+		double result = 0;
+		
+		System.out.println("Enter first number");
+		a=scan.nextDouble();
+		
+		System.out.println("Enter second number");
+		b=scan.nextDouble();
+		
+		System.out.println("Enter an operator: +, -, *, /");
+		String operator =scan.next();
+		
+		switch(operator) {
+		case "+": result=a+b;
+		break;
+		
+		case "-": result=a-b;
+		
+		  break;
+		  
+		case "*": result=a*b;
+		  break;
+		  
+		case "/": result=a/b;
+		default: System.out.println("Wrong operator");
+		 break;
+			
+		}
+		
+		System.out.println(a+" "+operator+" "+b+" "+" : "+result);
+		
+	}
+
+}
 
